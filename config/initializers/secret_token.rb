ebf4522f90386de2535043fe64be27fd18484c88
Matthew Marzin <mwmarzin@gmail.com>
2013-06-09 19:33:33 -0400

    Merge branch 'master' of https://github.com/mwmarzin/DrinkChewGuru
    Conflicts:
    	.gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DrinkChewGuru::Application.config.secret_token = '9808be9652b65e6a29b86cf833f7167744583a4ae056a6d24858c9c195bc337c67f4865f87c09be8cf0fc365c5a6b9a2bfd59a5f5fa89ac4d3e28488f1725b09'
