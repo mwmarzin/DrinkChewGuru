8495047e82676b693a8f61143134c4fae1257d09
Matthew Marzin <mwmarzin@gmail.com>
2013-06-18 20:32:29 -0400

    facebook test

--- lib/FacebookProvider.rb
+++ lib/FacebookProvider.rb
@@ -9,25 +9,5 @@ class FacebookProvider < Provider
     @state = rand(99999)
     @perms = "create_event"
   end
-  
-  def getOAuthTokenRequestURL()
-    @request = "#{@access_url}?" +
-      "client_id=#{@client_id}" +
-      "&redirect=#{@redirect_uri}" +
-      "&state=#{@state}"+
-      "&scope=#{@perms}"
-  end
-  
-  def getOAuthParamArray(scope = nil)
-    
-    a = {"client_id" => "#{@access_url}",
-         "redirect" => "#{@redirect_uri}",
-         "state" => "#{@state}"
-        }
-    if !scope.nil? 
-      a["scope"] = "#{scope}"
-    end
-    a
-  end
 end
   
--- lib/Provider.rb
+++ lib/Provider.rb
@@ -4,4 +4,24 @@ class Provider
   def initalize ()
     @state = rand(99999)
   end
+  
+  def getOAuthTokenRequestURL()
+    @request = "#{@access_url}?" +
+      "client_id=#{@client_id}" +
+      "&redirect_uri=#{@redirect_uri}" +
+      "&state=#{@state}"+
+      "&scope=#{@perms}"
+  end
+  
+  def getOAuthParamArray(scope = nil)
+    
+    a = {"client_id" => "#{@access_url}",
+         "redirect" => "#{@redirect_uri}",
+         "state" => "#{@state}"
+        }
+    if !scope.nil? 
+      a["scope"] = "#{scope}"
+    end
+    a
+  end
 end
