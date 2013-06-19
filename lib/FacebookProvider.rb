9178850abd6b28a847be0fd97912a52345c3dd37
Matthew Marzin <mwmarzin@gmail.com>
2013-06-18 20:53:27 -0400

    blarg2
    the rails.root thing didn't work like I had hoped

--- lib/FacebookProvider.rb
+++ lib/FacebookProvider.rb
@@ -4,7 +4,7 @@ class FacebookProvider < Provider
     super()
     @client_id = "181012372060361"
     @client_secret = "6e799ece526d2b8c9b9645a575eeaa84"
-    @redirect_uri = "#{Rails.root.to_s}/oauth/Facebook/callback"
+    @redirect_uri = "http://drinkchewguru.elasticbeanstalk.com/oauth/Facebook/callback"
     @access_url = "https://www.facebook.com/dialog/oauth/"
     @state = rand(99999)
     @perms = "create_event"
