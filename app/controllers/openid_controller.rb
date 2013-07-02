a6dd70dbbd436fba23693cb35ba5f2a73343778f
Anusha Chenreddy <anusha.chenreddy@gmail.com>
2013-07-02 16:45:48 -0400

    OpenId Controller changes

--- app/controllers/openid_controller.rb
+++ app/controllers/openid_controller.rb
@@ -2,7 +2,8 @@ require "net/http"
 require "uri"
 require "httpclient"
 require "net/https"
-
+require "rexml/document"
+include REXML
 
 
 class OpenidController < ApplicationController
@@ -18,20 +19,41 @@ class OpenidController < ApplicationController
 def getOpenIdXRD
 		user = params[:user]
 		print user+"\n"
-#		xrdurl = "http://www.google.com/s2/webfinger/?q="+user+
-#		print xrdurl
-		print "Hello...11\n"
+
 		http = HTTPClient.new
-		print "Hello...12\n"
-		#http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-		#response1 = http.get "http://www.google.com/s2/webfinger/?q=teju.chenreddy@gmail.com"
-#		response1 = http.get "https://www.google.com/accounts/o8/ud"
-		#response1 = http.get "https://accounts.google.com/o/openid2/auth","Content-Type" => "application/xrds+xml"
 		response1 = http.get "https://www.google.com/accounts/o8/id","Content-Type" => "application/xrds+xml"
-		print "Hello...13\n"
-		print response1.content
-		@response = response1.content+''
+		res = response1.content
+		print res
+#		xmldoc = Document.new res
+#		root = xmldoc.root
+#		xrduri = root.elements["XRD/Service/URI"].get_text
+		xrduri = extractXRDURI(res)
+		print "XRD URI of the document is \n"
+		print xrduri
+		print "\n"	
+		@postResponse = redirect_to(constructURL(xrduri))
+		print "\n"
+		end
+	
+
+	def extractXRDURI(res)
+		xmldoc = Document.new res
+		root = xmldoc.root
+		xrduri = root.elements["XRD/Service/URI"].get_text
 	end
+	
+   def constructURL(xrdurl)
+		parameters = "?openid.ns=http://specs.openid.net/auth/2.0
+&openid.claimed_id=http://specs.openid.net/auth/2.0/identifier_select
+&openid.identity=http://specs.openid.net/auth/2.0/identifier_select
+&openid.return_to=http://drinkchewguru.elasticbeanstalk.com/oauth
+&openid.realm=http://drinkchewguru.elasticbeanstalk.com/oauth
+&openid.assoc_handle=ABSmpf6DNMw
+&openid.mode=checkid_setup
+&openid.ui.mode=popup"
+		authURL = xrdurl.to_s()+parameters.to_s()
+   end
+
 	
 
 end
