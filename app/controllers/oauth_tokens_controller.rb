a130d4b3db05863a7be53716ed1ffb21287215c1
Matthew Marzin <mwmarzin@gmail.com>
2013-07-05 18:50:56 -0400

    uncommenting that was a bad idea

--- app/controllers/oauth_tokens_controller.rb
+++ app/controllers/oauth_tokens_controller.rb
@@ -20,7 +20,7 @@ class OauthTokensController < ApplicationController
 
   # POST /oauth_tokens
   def create
-    begin
+    #begin
       #TODO:need code for validating the state!!!
       @provider = getProviderClass(params[:provider])
     
@@ -64,11 +64,11 @@ class OauthTokensController < ApplicationController
         format.html # create.html.erb
       end
         
-      rescue => e
-        redirect_to(:controller => "oauth_tokens", :action =>"index", :error => "Sorry! We encountered an error getting data from #{params[:provider]}. If thise continues. Please contact an admin.")
-        logger.info 'ERROR on create in oauth tokens'
-        logger.info e.message
-      end
+      #rescue => e
+        #redirect_to(:controller => "oauth_tokens", :action =>"index", :error => "Sorry! We encountered an error getting data from #{params[:provider]}. If thise continues. Please contact an admin.")
+        #logger.info 'ERROR on create in oauth tokens'
+        #logger.info e.message
+        #end
     end
 
     def getProviderClass(providerName)
