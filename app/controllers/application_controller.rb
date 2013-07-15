1b431c74e5c4bc4119ff53030e80f1a5a07a4289
Matthew Marzin <mwmarzin@gmail.com>
2013-07-15 18:43:09 -0400

    start of events new page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,9 @@ class ApplicationController < ActionController::Base
       @user = User.find(session[:userid])
       @tokensHash = @user.oauth_tokens.index_by(&:provider)
       
+      @user.oauth_tokens.each do |token|
+        
+      end
       #TODO could check in here if the users tokens are still valid, if they aren't call another method to refresh the token
       #if the refresh fails, redirect to "/oauth"
     end
