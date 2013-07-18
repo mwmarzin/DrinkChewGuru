a8a292d9b0569041889961075a7193bb246a6a82
sherinab <ab.sherin@gmail.com>
2013-07-18 15:36:20 -0400

    added belongs to user in events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 #this is the event model, used for storing events in our database
 class Event < ActiveRecord::Base
+    belongs_to :user
   attr_accessible :user_id, :name, :date_time, :email_invitees, :facebook_id, :facebook_invitees, :description, :google_id, :loacation_id
 end
